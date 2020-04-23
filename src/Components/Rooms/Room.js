79c524fce5d9d1adcacc9256d834cf225d0d6854
Tijs <tijs.martens@student.hogent.be>
2020-04-23 08:25:46 +0200

    owner can see visitor

--- src/Components/Rooms/Room.js
+++ src/Components/Rooms/Room.js
@@ -11,67 +11,131 @@ function Room({ isOwner }) {
 
   const [yourVideoStream, setYourVideoStream] = useState();
 
+  const [callAccepted, setCallAccepted] = useState(false);
+
+  const [partnerSignal, setPartnerSignal] = useState();
+
   const yourVideo = useRef();
   const partnerVideo = useRef();
   const socket = useRef();
   useEffect(() => {
-    socket.current = io.connect("http://localhost:8000");
+    socket.current = io.connect("http://localhost:8001");
 
     navigator.mediaDevices
-      .getUserMedia({video: true, audio: false})
-      .then(stream => {
-        setYourVideoStream(stream)
-        if(yourVideo.current){
-          yourVideo.current.srcObject = stream
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        setYourVideoStream(stream);
+        if (yourVideo.current) {
+          yourVideo.current.srcObject = stream;
         }
-      })
+      });
 
-
-    socket.current.on('yourSocketId',(id) => {
-      setYourSocketId(id)
-      if(isOwner){
-        setRoomOwnerId(id)
-        socket.current.emit('setOwnerId', id)
-      }else{
-        setRoomVisitorId(id)
-        socket.current.emit('setVisitorId', id)
+    socket.current.on("yourSocketId", (id) => {
+      setYourSocketId(id);
+      if (isOwner) {
+        setRoomOwnerId(id);
+        socket.current.emit("setOwnerId", id);
+      } else {
+        setRoomVisitorId(id);
+        socket.current.emit("setVisitorId", id);
       }
-    })
+    });
 
-    socket.current.on('users', users => {
-      if(isOwner){
-        setRoomVisitorId(users.visitorId?users.visitorId:'the visitor is not yet online')
-        setPartnerSocketid(users.visitorId?users.visitorId:'the visitor is not yet online')
+    socket.current.on("users", (users) => {
+      if (isOwner) {
+        setRoomVisitorId(users.visitorId ? users.visitorId : "not online");
+        setPartnerSocketid(users.visitorId ? users.visitorId : "not online");
+      } else {
+        setRoomOwnerId(users.ownerId ? users.ownerId : "not online");
+        setPartnerSocketid(users.ownerId ? users.ownerId : "not online");
       }
-      else{
-        setRoomOwnerId(users.ownerId?users.ownerId:'the room owner is not yet online')
-        setPartnerSocketid(users.ownerId?users.ownerId:'the room owner is not yet online')
-      }
-    })
+    });
 
-    if(isOwner){
-      socket.current.on('visitorOnline', (id) => {
-        setRoomVisitorId(id)
-        setPartnerSocketid(id)
-        console.log('the visitor is online!!')
-      })
-    }else{
-      socket.current.on('ownerOnline', (id) => {
-        setRoomOwnerId(id)
-        setPartnerSocketid(id)
-        console.log('the owner is online!!')
-      })
+    if (isOwner) {
+      socket.current.on("visitorOnline", (id) => {
+        setRoomVisitorId(id);
+        setPartnerSocketid(id);
+        console.log("the visitor is online!!");
+      });
+    } else {
+      socket.current.on("ownerOnline", (id) => {
+        setRoomOwnerId(id);
+        setPartnerSocketid(id);
+        console.log("the owner is online!!");
+      });
     }
+
+
+    socket.current.on('hey', data => {
+      console.log('in hey', data.signal)
+      setPartnerSignal(data.signal)
+    })  
+
     // eslint-disable-next-line
   }, []);
 
+  const startConversation = () => {
+    console.log("initiate the call");
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: yourVideoStream,
+    });
 
+    peer.on("signal", (data) => {
+      socket.current.emit("callUser", {
+        userToCall: partnerSocketId, //id of the user to call
+        signalData: data, //our signal data
+        from: yourSocketId, //the receiver want's to know who is calling, so we pass our ID as wel
+      });
+    });
 
+    peer.on("stream", (stream) => {
+      // displaying the video of the other person
+      if (partnerVideo.current) {
+        partnerVideo.current.srcObject = stream;
+      }
+    });
 
+    socket.current.on("callAccepted", (signal) => {
+      console.log('call accepted')
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+  };
 
-  let yourVideoEelement
-  if(yourVideoStream){
-    yourVideoEelement = <video playsInline ref={yourVideo} autoPlay />
+  const acceptCall = () => {
+    setCallAccepted(true);
+
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: yourVideoStream
+    })
+
+    peer.on('signal', data => {
+      socket.current.emit('acceptCall',{
+        signal: data,
+        to: roomOwnerId
+      })
+    })
+
+    peer.on('stream', stream => {
+      console.log('stream', stream)
+      console.log(partnerVideo)
+    })
+
+    peer.signal(partnerSignal)
+  }
+
+  let yourVideoElement;
+  if (yourVideoStream) {
+    yourVideoElement = <video playsInline ref={yourVideo} autoPlay />;
+  }
+
+  let partnerVideoElement;
+  if (callAccepted) {
+    partnerVideoElement = <video playsInline ref={partnerVideo} autoPlay />;
   }
 
   return (
@@ -79,13 +143,19 @@ function Room({ isOwner }) {
       <h1> chatroom</h1>
       <div>your socket: {yourSocketId}</div>
       <div>partner socket: {partnerSocketId}</div>
-
+      <hr></hr>
       <div>room owner id: {roomOwnerId}</div>
       <div>room visitor id: {roomVisitorId}</div>
+      <hr></hr>
       <div>is owner: {isOwner.toString()}</div>
-      
+      {isOwner && yourSocketId && partnerSocketId && partnerSocketId !== "not online" && (
+        <button onClick={startConversation}>startConversation</button>
+      )}
+
+      {partnerSignal&& <button onClick={acceptCall}>start call with room owner</button>}
       <h2>your video:</h2>
-      {yourVideoEelement}
+      {yourVideoElement}
+      {partnerVideoElement}
     </div>
   );
 }
